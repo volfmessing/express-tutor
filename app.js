1ad64def29c3babf2cb9bf6e720df82d98f4df97
Vitos <vitalii.khodak@gmail.com>
2017-05-21 21:19:37 +0300

    created: default page response;

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ var express = require('express');
 
 var app = express();
 
+//routes
+
+
+//home
+app.get('/', function (req, res) {
+    res.send('Tutor app is running...');
+})
+
 app.listen(3000);
 console.log("Express is running...");
