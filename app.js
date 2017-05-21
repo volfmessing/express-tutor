06cf560d7d689770a1199f8ea11fa4f80549a711
Vitos <vitalii.khodak@gmail.com>
2017-05-21 21:57:06 +0300

    added: processing of not-found-pages

--- app.js
+++ app.js
@@ -10,13 +10,17 @@ var app = express();
 
 //home
 app.get('/', function (req, res) {
-    res.send('Tutor app is running...');
+    res.send('<h2>Tutor app is running...</h2>');
 })
 //get episode pages
 app.get('/star_wars_episode/:episode_number', function (req, res) {
     var episode = req.params.episode_number;
     res.send("This is the page for episode: " + episode);
 });
+
+app.get('*', function (req, res) {
+    res.send('<h2>The Page  You are looking for is not found</h2>')
+})
 
 app.listen(3000, function () {
     console.log("Express is running on port 3000...");
